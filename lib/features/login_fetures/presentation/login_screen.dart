784e7e2b89364c890bbafd9890510a70044f0de1
Yousef Saleh <147569956+yousef2256@users.noreply.github.com>
2024-12-03 16:20:45 +0200

    11

--- lib/features/login_fetures/presentation/login_screen.dart
+++ lib/features/login_fetures/presentation/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:m_auto/core/helpers/spaser.dart';
-import 'package:m_auto/core/helpers/validations.dart';
 import 'package:m_auto/core/theme/app_colors.dart';
 import 'package:m_auto/core/utils/constets/images_constents.dart';
 import 'package:m_auto/core/utils/texts/text_styles.dart';
